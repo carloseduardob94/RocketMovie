6e23fae7e625bba3036930bd1e9458b0e6ae8c7c
Eduardo <educonts08@gmail.com>
2022-10-16 00:00:50 -0300

    styles created

---
+++ src/styles/theme.js
@@ -0,0 +1,14 @@
+export default {
+  COLORS: {
+    BACKGROUND_900: '#0D0C0F',
+    BACKGROUND_800: '#1C1B1E',
+    BACKGROUND_700: '#262529',
+
+    PINK: '#FF859B',
+    WHITE: '#E5E5E5',
+
+    GRAY_100: '#948F99',
+    GRAY_200: '#3E3B47',
+    GRAY_300: '#312E38'
+  }
+}
